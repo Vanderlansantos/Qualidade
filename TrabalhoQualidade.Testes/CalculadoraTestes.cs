2de431c5a49c991fdfc411eadc04084ad44770d4
Thelmaryo Vieira Lima <thelmaryo@hotmail.com>
2019-05-30 19:30:25 -0300

    Adição de um projeto de testes

---
+++ TrabalhoQualidade.Testes/CalculadoraTestes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TrabalhoQualidade.Testes
+{
+    [TestClass]
+    public class CalculadoraTestes
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ TrabalhoQualidade.Testes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TrabalhoQualidade.Testes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TrabalhoQualidade.Testes")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("efb786c4-eb23-4436-a282-3cfe062deb8b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
